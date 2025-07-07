f380a06a84610454d7e9a7e213280fadb1a7a249
Breno Pereira <brenodsp.rj@gmail.com>
2025-07-07 19:23:28 -0300

    [feat] Formatar colunas do DF de feriados para snake_case

--- inputs/data_handler.py
+++ inputs/data_handler.py
@@ -1,13 +1,13 @@
 from pandas import DataFrame, read_excel
 
+from utils.enums import Indices
+from utils.formatters import to_snake_case
+
 class InputsDataHandler:
     _INPUTS_PATH = "dados\Dados trabalho 2025.xlsx"
 
     def feriados(self) -> DataFrame:
-        return read_excel(self._INPUTS_PATH, sheet_name="Feriados Brasil").dropna(subset="Feriado")
-    
+        df = read_excel(self._INPUTS_PATH, sheet_name="Feriados Brasil").dropna(subset="Feriado")
+        df.columns = [to_snake_case(col) for col in df.columns]
 
-if __name__ == "__main__":
-    data_handler = InputsDataHandler()
-    feriados = data_handler.feriados()
-    teste = 0
+        return df
