956c818488bb015534cf6041ee0f1d61cf89881a
Breno Pereira <brenodsp.rj@gmail.com>
2025-07-07 23:58:45 -0300

    [feat] Extrair informações adicionais de ativos das posições, se necessário

--- core/carteira.py
+++ core/carteira.py
@@ -1,20 +1,39 @@
-from typing import Union
+from typing import Optional, Union
 
-from utils.enums import AcoesBr, AcoesUs, Opcoes, Titulos, Futuros
+from inputs.data_handler import InputsDataHandler
+from utils.enums import AcoesBr, AcoesUs, Opcoes, Titulos, Futuros, definir_tipo_futuro
 
 
 class Posicao:
-    def __init__(self, ativo: Union[AcoesBr, AcoesUs, Opcoes, Titulos, Futuros], quantidade: float):
-        self.ativo = ativo,
+    def __init__(
+            self, 
+            ativo: Union[AcoesBr, AcoesUs, Opcoes, Titulos, Futuros], 
+            quantidade: float,
+            inputs_data_handler: Optional[InputsDataHandler] = None
+    ):
+        self.ativo = ativo
         self.quantidade = quantidade
+        self.gerar_informacao_adicional(inputs_data_handler)
+
+    def gerar_informacao_adicional(self, inputs_data_handler: Optional[InputsDataHandler]) -> None:
+        # Forçar necessidade de prover inputs para certos ativos
+        if isinstance(self.ativo, Futuros) and not inputs_data_handler:
+            raise ValueError("Informações adicionais são necessárias, mas inputs não foram fornecidos.")
+        
+        # Extrair informações adicionais
+        if isinstance(self.ativo, Futuros):
+            df = inputs_data_handler.futuros()
+            tipo_futuro = df[df["id"] == self.ativo.value]["tipo"].values[0]
+            self.tipo_futuro = definir_tipo_futuro(tipo_futuro)
 
 
 class Carteira:
-    POSICAO_1 = Posicao(AcoesBr.EMBRAER, 1500)
-    POSICAO_2 = Posicao(AcoesBr.CASAS_BAHIA, 24500)
-    POSICAO_3 = Posicao(AcoesUs.FORD_MOTORS, 1700)
-    POSICAO_4 = Posicao(Opcoes.OPCAO_9, 1.5)
-    POSICAO_5 = Posicao(Futuros.FUTURO_15, 0.6)
-    POSICAO_6 = Posicao(Futuros.FUTURO_9, 0.2)
-    POSICAO_7 = Posicao(Futuros.FUTURO_25, 1700)
-    POSICAO_8 = Posicao(Titulos.TITULO_9, 250000)
+    def __init__(self, inputs_data_handler: InputsDataHandler):
+        self.POSICAO_1 = Posicao(AcoesBr.EMBRAER, 1500)
+        self.POSICAO_2 = Posicao(AcoesBr.CASAS_BAHIA, 24500)
+        self.POSICAO_3 = Posicao(AcoesUs.FORD_MOTORS, 1700)
+        self.POSICAO_4 = Posicao(Opcoes.OPCAO_9, 1.5)
+        self.POSICAO_5 = Posicao(Futuros.FUTURO_15, 0.6, inputs_data_handler)
+        self.POSICAO_6 = Posicao(Futuros.FUTURO_9, 0.2, inputs_data_handler)
+        self.POSICAO_7 = Posicao(Futuros.FUTURO_25, 1700, inputs_data_handler)
+        self.POSICAO_8 = Posicao(Titulos.TITULO_9, 250000)
