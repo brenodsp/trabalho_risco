9023f09c313ef0d743b6f8c58c3eb0f46a21a4bd
Breno Pereira <brenodsp.rj@gmail.com>
2025-07-10 12:47:03 -0300

    [feat] Calcular exposição para ações

--- core/fatores_risco/exposicao.py
+++ core/fatores_risco/exposicao.py
@@ -1,12 +1,15 @@
 from pandas import DataFrame
 
 from core.carteira import Posicao
-from utils.enums import FatoresRisco, Opcoes
+from core.fatores_risco.fatores_risco import nomear_vetor_fator_risco
+from inputs.data_handler import InputsDataHandler
+from utils.enums import FatoresRisco, Opcoes, Localidade, Colunas, TipoFuturo
 
 
 class Exposicao:
-    def __init__(self, posicao: Posicao):
+    def __init__(self, posicao: Posicao, inputs: InputsDataHandler):
         self.posicao = posicao
+        self.inputs = inputs
 
     def calcular_exposicao(self) -> DataFrame:
         exposicoes_fatores_risco = []
@@ -16,8 +19,28 @@ class Exposicao:
 
         for fr in self.posicao.fatores_risco:
             if fr == FatoresRisco.ACAO:
-                # TODO: implementar calculo de exposição simples para ações
-                pass
+                # Fazer distinção entre ações brasileiras e americanas
+                if self.posicao.localidade == Localidade.BR:
+                    precos = self.inputs.acoes_br()
+                    cambio = 1.0
+                elif self.posicao.localidade == Localidade.US:
+                    precos = self.inputs.acoes_br()
+                    df_cambio = self.inputs.fx()
+                    cambio = float(df_cambio.loc[
+                        (df_cambio[Colunas.DATA.value] == df_cambio[Colunas.DATA.value].max()) &
+                        (df_cambio[Colunas.CAMBIO.value] == TipoFuturo.USDBRL.name)
+                    ][Colunas.VALOR.value].values[0])
+                # Buscar último preco da ação
+                precos = self.inputs.acoes_br() if self.posicao.localidade == Localidade.BR else self.inputs.acoes_us()
+                ultimo_preco = precos.loc[
+                    (precos[Colunas.ATIVO.value] == self.posicao.ativo.value) &
+                    (precos[Colunas.DATA.value] == precos[Colunas.DATA.value].max())
+                ][Colunas.PRECO.value].values[0]
+
+                # Calcular exposição e adicionar ao vetor de exposições
+                w = float(self._exposicao_acao(self.posicao.quantidade, ultimo_preco))
+                exposicoes_fatores_risco.append(w)
+
             elif fr in [FatoresRisco.CAMBIO_USDBRL, FatoresRisco.CAMBIO_USDOUTROS]:
                 # TODO: implementar calculo de exposição para cambio
                 pass
@@ -26,10 +49,15 @@ class Exposicao:
                 pass
             else:
                 raise ValueError("Fator de risco desconhecido.")
+            
+        return DataFrame(
+            data=exposicoes_fatores_risco,
+            columns=nomear_vetor_fator_risco()
+        )
 
     @staticmethod
-    def _exposicao_acao(quantidade: float, preco: float, delta: float = 1.0) -> float:
-        return delta * quantidade * preco
+    def _exposicao_acao(quantidade: float, preco: float, delta: float = 1.0, cambio: float = 1.0) -> float:
+        return (delta * quantidade) * (preco * cambio)
     
     @staticmethod
     def _exposicao_volatilidade(quantidade: float, vega: float) -> float:
