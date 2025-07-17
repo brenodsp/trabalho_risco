6b68eb4b5e07a4a9787dec29f1cb421158efd0c3
Breno Pereira <brenodsp.rj@gmail.com>
2025-07-17 00:21:25 -0300

    [feat] Calcular PnL de ações e opções

--- core/var/var.py
+++ core/var/var.py
@@ -2,13 +2,15 @@ from datetime import date
 from math import sqrt
 
 from numpy import array
-from pandas import DataFrame
+from pandas import DataFrame, Series, to_datetime, concat
 
 from core.carteira import Carteira, Posicao
+from core.fatores_risco.black_scholes import bs_implied_vol, bs_price
 from core.fatores_risco.exposicao import ExposicaoCarteira
 from core.fatores_risco.fatores_risco import MatrizFatoresRisco, nomear_vetor_fator_risco
+from core.renda_fixa.renda_fixa import RendaFixa
 from inputs.data_handler import InputsDataHandler
-from utils.enums import IntervaloConfianca
+from utils.enums import IntervaloConfianca, AcoesBr, AcoesUs, Opcoes, Futuros, TipoFuturo, Titulos, Localidade, Colunas
 
 
 class VarParametrico:
@@ -56,10 +58,11 @@ class VarHistorico:
         self.retornos = retornos
         self.inputs = inputs
 
-    def _gerar_cenarios(self) -> DataFrame:
+    def _gerar_cenarios(self, n_cenarios: int) -> DataFrame:
         retornos = self.retornos.fatores_risco_carteira()
         posicoes = [p for p in self.carteira.__dict__.values() if isinstance(p, Posicao)]
         
+        lista_pnl_posicao = []
         for posicao in posicoes:
             # Filtrar retornos pertinentes à posição avaliada
             fatores_risco = [
@@ -67,10 +70,91 @@ class VarHistorico:
                 for fr in posicao.fatores_risco
             ]
             retornos_posicao = retornos.loc[:, fatores_risco]
+            retornos_posicao[Colunas.POSICAO.value] = posicao.ativo.name
 
             # Pegar valores de referência para a geração de cenários
-            # TODO: criar dataframes para cada tipo de ativo
-            # TODO: normalizar dataframes em um só formato contendo o P&L de cada fator de risco
+            if isinstance(posicao.ativo, AcoesBr) or isinstance(posicao.ativo, AcoesUs):
+                if posicao.localidade == Localidade.BR:
+                    ultimo_preco = self.inputs.acoes_br()
+                    cambio = 1.0
+                else:
+                    ultimo_preco = self.inputs.acoes_us()
+                    cambio = self.inputs.fx()
+                    cambio = float(cambio.loc[
+                        (cambio[Colunas.CAMBIO.value] == TipoFuturo.USDBRL.name) &
+                        (cambio[Colunas.DATA.value] == data_ref)
+                    ][Colunas.VALOR.value].values[0])
+                    
+                data_ref = ultimo_preco.loc[ultimo_preco[Colunas.DATA.value] <= to_datetime(self.carteira.data_referencia)][Colunas.DATA.value].max()
+                ultimo_preco = float(ultimo_preco.loc[
+                    (ultimo_preco[Colunas.ATIVO.value] == posicao.ativo.value) &
+                    (ultimo_preco[Colunas.DATA.value] == data_ref)
+                ][Colunas.PRECO.value].values[0])
+
+                retornos_posicao[Colunas.PNL.value] = self._calcular_pnl(
+                    posicao.quantidade, 
+                    cambio * ultimo_preco, 
+                    ultimo_preco * (1 + retornos_posicao[posicao.ativo.name])
+                )
+
+            elif isinstance(posicao.ativo, Opcoes):
+                # Recuperar informações de opções
+                acoes = self.inputs.acoes_br()
+                acoes = acoes.loc[acoes[Colunas.ATIVO.value] == posicao.produto.value][[Colunas.DATA.value, Colunas.PRECO.value]]
+                opcoes = self.inputs.opcoes()
+                opcoes = opcoes.loc[opcoes[Colunas.ID.value] == posicao.ativo.value]
+
+                # Definir características da opção
+                S = float(opcoes["strike"].values[0])
+                K = float(opcoes["nocional"].values[0])
+                T = RendaFixa.calcular_du(to_datetime(self.carteira.data_referencia), to_datetime(opcoes["vencimento"].values[0]), self.inputs)/252
+                preco_opcao = float(opcoes["preco"].values[0])
+                tipo = 1 if opcoes["tipo"].values[0] == "call" else -1
+
+                # Calcular propriedades Black-Scholes
+                vol_implicita = float(bs_implied_vol(S, K, T, 0, 0, preco_opcao, tipo, 1))
+                coluna_vol = [c for c in retornos_posicao.columns if "VOL" in c][0]
+
+                # Precificar cenários de preço da opção
+                retornos_posicao = retornos_posicao.merge(acoes, on=Colunas.DATA.value)
+                retornos_posicao["cenario_vol"] = retornos_posicao[coluna_vol] + vol_implicita/100
+                retornos_posicao["cenario_preco"] = retornos_posicao.apply(
+                    lambda row: bs_price(row[Colunas.PRECO.value], K, T-(1/252), 0, 0, row["cenario_vol"]*100, tipo, 1),
+                    axis=1
+                )
+                
+                # Produzir cenários de P&L
+                retornos_posicao[Colunas.PNL.value] = self._calcular_pnl(
+                    posicao.quantidade, 
+                    preco_opcao, 
+                    retornos_posicao["cenario_preco"]
+                )
+            
+            # elif futuro_di:
+            
+            # elif futuro_cambio:
+            
+            # elif futuro_indice:
+            
+            # elif titulo:
+            
+            else:
+                raise ValueError("Ativo não mapeado.")
+
+            df_posicao = retornos_posicao[[
+                Colunas.DATA.value,
+                Colunas.POSICAO.value,
+                Colunas.PNL.value
+            ]]
+
+            lista_pnl_posicao.append(df_posicao)
+        
+        return concat(lista_pnl_posicao)
+            
+
+    @staticmethod
+    def _calcular_pnl(qtd: float, preco_referencia: float, preco_cenario: Series) -> Series:
+        return qtd * (preco_cenario - preco_referencia)
 
     def var_historico_carteira(self, n_cenarios: int) -> float:
         # TODO: LEMBRANDO QUE O VAR DEVE SER UM VALOR ABSOLUTO
